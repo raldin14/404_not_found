025044caa7a167311cb780c0db6e9a2dbc9a4342
Raldin <raldin.hidalgo@gmail.com>
2021-05-27 23:11:45 -0400

    getting more done

--- src/js/actions.js
+++ src/js/actions.js
@@ -7,26 +7,38 @@ function balance(vals){
         return;
     }
 
-    for(let i = 0; i <= vals.length; i++){
-        if(vals.length%2 == 0){
-            let r = vals.length;
-            alert(r);
-            if(r == 0 || r == 1){
-                alert(true);
-            }else if(vals[i] == ''){
-                alert(true);
-            }else{
-                alert(false);
-            }
-        }else if(vals.length == 0 || vals.length == 1){
-            alert(true);
-        }else{
-            alert(false);
+    let firstVal = vals.substring(0,vals.length/2);
+    let secondVal = vals.substring(vals.length/2,vals.length);
+    alert(firstVal);
+    alert(secondVal);
+    let wildCard = 0;
+    let matches = 0;
+    for(let w = 0; w < vals.length; w++){
+        if(vals[w] == "*"){
+            wildCard++;
         }
+    }
+
+    for(let i = 0; i < vals.length/2; i++){
+        for(let j = 0; j < vals.length/2; j++){
+            if(firstVal[i] == secondVal[j]){
+                matches++;
+            }else if(wildCard > 0){
+                if(firstVal[i] == "*" || secondVal[j] == "*"){
+                    wildCard--;
+                    matches++;
+                }
+            }
+        }
+    }
+
+    if(matches == vals.length/2){
+        alert(true);
+    }else{
+        alert(false);
     }
 }
 
 function balanced(){
     balance(document.getElementById("vals").value);
 }
-
