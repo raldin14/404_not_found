b5ebe4f836c32172ffa1a6d863d8a6ad3e6ed8d1
Raldin D. Hidalgo Tirado <rhidalgo@bsc.com.do>
2021-05-28 11:32:02 -0400

    first commit

--- src/js/actions.js
+++ src/js/actions.js
@@ -1,6 +1,11 @@
 function balance(vals){
     let wildCard = 0;
     let matches = 0;
+    let firstTotal = 0;
+    let secondTotal = 0;
+    let firstVal = vals.substring(0,vals.length/2);
+    let secondVal = vals.substring(vals.length/2,vals.length);
+    let val = vals;
     if(vals.length == 0 || vals.length == 1 || vals.length == 2){
         alert(true);
         return;
@@ -19,12 +24,8 @@ function balance(vals){
         }else{
             alert(true);
             return;
-        }
-        
+        }        
     }
-
-    let firstVal = vals.substring(0,vals.length/2);
-    let secondVal = vals.substring(vals.length/2,vals.length);
     alert(firstVal);
     alert(secondVal);
 
@@ -35,7 +36,30 @@ function balance(vals){
     }
 
     for(let i = 0; i < vals.length/2; i++){
-        for(let j = 0; j < vals.length/2; j++){
+        firstTotal = firstVal.split(firstVal[i]).length -1;
+        secondTotal = secondVal.split(firstVal[i]).length -1;
+        alert(firstTotal);
+        alert(secondTotal);
+        if(firstTotal == secondTotal){
+            if(val.split(firstVal[i]).length -1 > 0){
+                val = val.replaceAll(firstVal[i],"");
+            }
+            //val = val.replaceAll(secondVal[i],"");
+            matches++;
+        }else if(wildCard > 0){
+            if(firstVal[i] == "*" || secondVal[j] == "*"){
+                wildCard--;
+                matches++;
+                val = val.replaceAll("*","");
+                //val = val.replaceAll("*","");
+            }
+        }
+
+        if(val.length == 0){
+            alert(true);
+            return;
+        }
+       /* for(let j = 0; j < vals.length/2; j++){
             if(firstVal[i] == secondVal[j]){
                 matches++;
             }else if(wildCard > 0){
@@ -44,14 +68,18 @@ function balance(vals){
                     matches++;
                 }
             }
-        }
+        }*/
     }
 
-    if(matches == vals.length/2){
+    if(val.length > 0){
+        alert(false);
+        return;
+    }
+    /*if(matches == vals.length/2){
         alert(true);
     }else{
         alert(false);
-    }
+    }*/
 }
 
 function balanced(){
