f64fd6dc34825d6392aab98b1bd528e7552d8814
Raldin Hidalgo <raldin.hidalgo@gmail.com>
2021-05-28 06:53:01 -0400

    adding js

--- src/js/actions.js
+++ src/js/actions.js
@@ -1,18 +1,33 @@
 function balance(vals){
-    if(vals.length == 0 || vals.length == 1){
+    let wildCard = 0;
+    let matches = 0;
+    if(vals.length == 0 || vals.length == 1 || vals.length == 2){
         alert(true);
         return;
     }else if(vals.length%2 != 0){
-        alert(false);
-        return;
+        for(let i = 0; i < vals.length/2; i++){
+            for(let j = 0; j < vals.length/2; j++){
+                if(firstVal[i] == secondVal[j]){
+                    matches++;
+                }
+            }
+        }
+
+        if(matches > 0){
+            alert(false);
+            return;
+        }else{
+            alert(true);
+            return;
+        }
+        
     }
 
     let firstVal = vals.substring(0,vals.length/2);
     let secondVal = vals.substring(vals.length/2,vals.length);
     alert(firstVal);
     alert(secondVal);
-    let wildCard = 0;
-    let matches = 0;
+
     for(let w = 0; w < vals.length; w++){
         if(vals[w] == "*"){
             wildCard++;
